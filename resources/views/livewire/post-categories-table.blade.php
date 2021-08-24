73eca8f8b955f75981aac79f9c4fa721711c5dd6
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-08-24 08:37:02 +0200

    random

--- resources/views/livewire/post-categories-table.blade.php
+++ resources/views/livewire/post-categories-table.blade.php
@@ -26,55 +26,25 @@
                         <td>{{$postcategory->updated_at->diffForHumans()}}</td>
                         <td class="text-center">
                             <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-alt-secondary"   data-bs-toggle="modal" data-bs-target="#exampleModal{{$postcategory->id}}" title="Edit Client">
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-sm btn-alt-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal{{$postcategory->id}}">
                                     <i class="fa fa-fw fa-pencil-alt"></i>
                                 </button>
+
                                 <button wire:click="removeCategory({{$postcategory->id}})" type="button" class="btn btn-sm btn-alt-secondary" data-bs-toggle="tooltip" title="Remove Client">
                                     <i class="fa fa-fw fa-times"></i>
                                 </button>
-                                <div wire:ignore.self class="modal fade" id="exampleModal{{$postcategory->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header ">
-                                                <h5 class="modal-title d-flex align-items-center text-dark" id="exampleModalLabel">Edit postcategory</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form wire:submit.prevent="submitFormpostcategory({{$postcategory->id}})">
-                                                    <div class="modal-body">
-                                                        <div  class="row">
-                                                            <div class="col-12">
-                                                                <input id="input1" name="name" type="text" class="form-control my-1 styleinput" placeholder="{{$postcategory->name}}" aria-label="Username" aria-describedby="basic-addon1" wire:model="name">
-                                                                @error('name')
-                                                                <p class="text-danger"> {{ $message }}</p>
-                                                                @enderror
-                                                            </div>
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                            <button type="submit" class="btn btn-dark">Save</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Launch demo modal
-                                </button>
 
                                 <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal fade" id="exampleModal{{$postcategory->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                <h5 class="modal-title" id="exampleModalLabel">Update Category</h5>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
-                                            <div class="modal-body">
+                                            <div class="modal-body d-flex justify-content-start">
                                                 {!! Form::open(['method'=>'PATCH', 'action'=>['App\Http\Controllers\AdminPostCategoryController@update',$postcategory->id],
                                                     'files'=>false])!!}
                                                 <div class="form-group">
@@ -82,7 +52,7 @@
                                                     {!! Form::text('name',$postcategory->name,['class'=>'form-control']) !!}
                                                 </div>
                                                 <div class="form-group mr-1">
-                                                    {!! Form::submit('Update Category',['class'=>'btn btn-primary']) !!}
+                                                    {!! Form::submit('Update Category',['class'=>'btn btn-secondary']) !!}
                                                 </div>
                                                 {!! Form::close() !!}
                                             </div>
