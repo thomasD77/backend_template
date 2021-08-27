9617be6c344ab139f4d3bc7b85b46b2a7f3c5268
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-08-27 08:41:03 +0200

    general

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -81,8 +81,7 @@
             </div>
         </div>
         <div class="block-content">
-            <table class="table table-borderless">
-                <tbody>
+            <div class="table table-borderless">
                 @foreach($post->comments->where('reply_id', null) as $comment)
                 <tr class="bg-body-dark mt-5">
                     <td class="d-none d-sm-table-cell"></td>
@@ -106,37 +105,6 @@
                         </div>
                     </td>
                 </tr>
-
-                <table class="table table-borderless row">
-                    <tbody class="col-lg-10 offset-lg-1">
-                    @foreach($post->comments->where('reply_id', $comment->id) as $commentReply)
-                        <tr class="bg-body-light mt-5">
-                            <td class="d-none d-sm-table-cell"></td>
-                            <td class="fs-sm text-muted">
-                                <a class="fw-semibold" href="be_pages_generic_profile.php">{{ $commentReply->user->name }} on <span class="text-muted ms-4">{{ $commentReply->created_at->diffForHumans() }}</span></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="d-none d-sm-table-cell text-center" style="width: 140px;">
-
-                                <img class="rounded-circle" height="62" width="62" src="{{ $commentReply->user->avatar ? asset('/') . $commentReply->user->avatar->file : 'http://placehold.it/62x62'  }}" alt="">
-
-                                <p class="fs-sm fw-medium mt-3">{{ $commentReply->user->posts->count() . 'Posts' }}</p>
-                            </td>
-                            <td>
-                                {!! $commentReply->body !!}
-                                <hr>
-                                <div class="d-flex justify-content-between">
-                                    <p class="fs-sm text-muted">There is only one way to avoid criticism: do nothing, say nothing, and be nothing.</p>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-
-
-
                 <!-- Large Block Modal -->
                 <div class="modal" id="modal-block-large" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
                     <div class="modal-dialog modal-lg" role="document">
@@ -163,63 +131,173 @@
                     </div>
                 </div>
                 <!-- END Large Block Modal -->
+
+
+                    @foreach($post->comments->where('reply_id', $comment->id) as $commentReply)
+                        <tr class="bg-body-light mt-5">
+                            <td class="d-none d-sm-table-cell"></td>
+                            <td class="fs-sm text-muted">
+                                <a class="fw-semibold" href="be_pages_generic_profile.php">{{ $commentReply->user->name }} on <span class="text-muted ms-4">{{ $commentReply->created_at->diffForHumans() }}</span></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="d-none d-sm-table-cell text-center" style="width: 140px;">
+
+                                <img class="rounded-circle" height="62" width="62" src="{{ $commentReply->user->avatar ? asset('/') . $commentReply->user->avatar->file : 'http://placehold.it/62x62'  }}" alt="">
+
+                                <p class="fs-sm fw-medium mt-3">{{ $commentReply->user->posts->count() . 'Posts' }}</p>
+                            </td>
+                            <td>
+                                {!! $commentReply->body !!}
+                                <hr>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fs-sm text-muted">There is only one way to avoid criticism: do nothing, say nothing, and be nothing.</p>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+
                 @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
 
-
-    <!-- END Discussion -->
-
-    <!-- Comment -->
-    <div class="block block-rounded">
-        <div class="block-header block-header-default">
-            <h3 class="block-title">Please share your thoughts with us!</h3>
-            <div class="block-options">
-                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="fullscreen_toggle"></button>
             </div>
         </div>
-        <div class="block-content">
-            <table class="table table-borderless">
-                <tbody>
-                <tr class="table-active" id="forum-reply-form">
-                    <td class="d-none d-sm-table-cell"></td>
-                    <td class="fs-sm text-muted">
-                        <a class="fw-semibold" href="be_pages_generic_profile.php"><?php echo Auth::user()->name ?></a> type your comment
-                    </td>
-                </tr>
-                <tr>
-                    <td class="d-none d-sm-table-cell text-center">
-                        <p>
-                            <img class="rounded-circle" height="62" width="62" src="<?php echo Auth::user()->avatar ? asset('/') . Auth::user()->avatar->file : 'http://placehold.it/62x62' ?>" alt="">
-
-                        </p>
-                        <p class="fs-sm fw-medium"><?php echo (Auth::user()->posts->count() . " " . 'Posts') ?></p>
-                    </td>
-                    <td>
-                        {!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\AdminCommentController@store']) !!}
-                        <div class="form-group  mb-4">
-                            {!! Form::textarea('body',null,['class'=>'form-control', 'id'=>'js-ckeditor5-classic']) !!}
-                            @error('body')
-                            <p class="text-danger mt-2"> {{ $message }}</p>
-                            @enderror
-                            {{ Form::hidden('post_id', $post->id) }}
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <div class="form-group mr-1">
-                                {!! Form::button('<i class="far fa-comment me-1 opacity-50"></i> Comment',['type'=>'submit','class'=>'btn btn-alt-primary']) !!}
-                            </div>
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
     </div>
+    <!-- END Discussion -->
+
+
+
+
+
+
+{{--    <!-- Comment -->--}}
+{{--    <div class="block block-rounded">--}}
+{{--        <div class="block-header block-header-default">--}}
+{{--            <h3 class="block-title">Please share your thoughts with us!</h3>--}}
+{{--            <div class="block-options">--}}
+{{--                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="fullscreen_toggle"></button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="block-content">--}}
+{{--            <table class="table table-borderless">--}}
+{{--                <tbody>--}}
+{{--                <tr class="table-active" id="forum-reply-form">--}}
+{{--                    <td class="d-none d-sm-table-cell"></td>--}}
+{{--                    <td class="fs-sm text-muted">--}}
+{{--                        <a class="fw-semibold" href="be_pages_generic_profile.php"><?php echo Auth::user()->name ?></a> type your comment--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--                <tr>--}}
+{{--                    <td class="d-none d-sm-table-cell text-center">--}}
+{{--                        <p>--}}
+{{--                            <img class="rounded-circle" height="62" width="62" src="<?php echo Auth::user()->avatar ? asset('/') . Auth::user()->avatar->file : 'http://placehold.it/62x62' ?>" alt="">--}}
+
+{{--                        </p>--}}
+{{--                        <p class="fs-sm fw-medium"><?php echo (Auth::user()->posts->count() . " " . 'Posts') ?></p>--}}
+{{--                    </td>--}}
+{{--                    <td>--}}
+{{--                        {!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\AdminCommentController@store']) !!}--}}
+{{--                        <div class="form-group  mb-4">--}}
+{{--                            {!! Form::textarea('body',null,['class'=>'form-control', 'id'=>'js-ckeditor5-classic']) !!}--}}
+{{--                            @error('body')--}}
+{{--                            <p class="text-danger mt-2"> {{ $message }}</p>--}}
+{{--                            @enderror--}}
+{{--                            {{ Form::hidden('post_id', $post->id) }}--}}
+{{--                        </div>--}}
+{{--                        <div class="d-flex justify-content-end">--}}
+{{--                            <div class="form-group mr-1">--}}
+{{--                                {!! Form::button('<i class="far fa-comment me-1 opacity-50"></i> Comment',['type'=>'submit','class'=>'btn btn-alt-primary']) !!}--}}
+{{--                            </div>--}}
+{{--                        {!! Form::close() !!}--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--                </tbody>--}}
+{{--            </table>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <!-- END Discussion -->
 </div>
 <!-- END Page Content -->
+
+<div class="content content-boxed">
+    <div class="block block-rounded">
+        <div class="block-header block-header-default">
+            <h3 class="block-title">Post with all Comments!</h3>
+            <div class="block-options">
+                <a class="btn-block-option me-2" href="#forum-reply-form">
+                    <i class="far fa-comment"></i> Comment
+                </a>
+                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="fullscreen_toggle"></button>
+                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
+                    <i class="si si-refresh"></i>
+                </button>
+            </div>
+        </div>
+        <div class="block-content">
+            @foreach($post->comments->where('reply_id', null) as $comment)
+                <tr class="bg-body-dark mt-5">
+                    <td class="d-none d-sm-table-cell"></td>
+                    <td class="fs-sm text-muted">
+                        <a class="fw-semibold" href="be_pages_generic_profile.php">{{ $comment->user->name }} on <span class="text-muted ms-4">{{ $comment->created_at->diffForHumans() }}</span></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="d-none d-sm-table-cell text-center" style="width: 140px;">
+
+                        <img class="rounded-circle" height="62" width="62" src="{{ $comment->user->avatar ? asset('/') . $comment->user->avatar->file : 'http://placehold.it/62x62'  }}" alt="">
+
+                        <p class="fs-sm fw-medium mt-3">{{ $comment->user->posts->count() . 'Posts' }}</p>
+                    </td>
+                    <td>
+                        {!! $comment->body !!}
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <p class="fs-sm text-muted">There is only one way to avoid criticism: do nothing, say nothing, and be nothing.</p>
+                            <button type="button" class="btn btn-alt-primary push" data-bs-toggle="modal" data-bs-target="#modal-block-large"><i class="fa fa-reply me-1"></i>Reply</button>
+                        </div>
+                    </td>
+                </tr>
+                <div>
+                    {!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\AdminCommentController@storeReply']) !!}
+                    <div>
+                        {!! Form::textarea('body',null,['class'=>'form-control']) !!}
+                    </div>
+
+                    {{ Form::hidden('comment_id', $comment->id) }}
+                    {{ Form::hidden('post_id', $post->id) }}
+                    {!! Form::button('<i class="fa fa-reply me-1"></i>Reply',['type'=>'submit','class'=>'btn btn-alt-primary my-3']) !!}
+                    {!! Form::close() !!}
+                </div>
+                @foreach($post->comments->where('reply_id', $comment->id) as $commentReply)
+                    <tr class="bg-body-light mt-5">
+                        <td class="d-none d-sm-table-cell"></td>
+                        <td class="fs-sm text-muted">
+                            <a class="fw-semibold" href="be_pages_generic_profile.php">{{ $commentReply->user->name }} on <span class="text-muted ms-4">{{ $commentReply->created_at->diffForHumans() }}</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="d-none d-sm-table-cell text-center" style="width: 140px;">
+
+                            <img class="rounded-circle" height="62" width="62" src="{{ $commentReply->user->avatar ? asset('/') . $commentReply->user->avatar->file : 'http://placehold.it/62x62'  }}" alt="">
+
+                            <p class="fs-sm fw-medium mt-3">{{ $commentReply->user->posts->count() . 'Posts' }}</p>
+                        </td>
+                        <td>
+                            {!! $commentReply->body !!}
+                            <hr>
+                            <div class="d-flex justify-content-between">
+                                <p class="fs-sm text-muted">There is only one way to avoid criticism: do nothing, say nothing, and be nothing.</p>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+        </div>
+
+
+        @endforeach
+    </div>
+
+</div>
+
 
 
 
